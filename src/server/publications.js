ad3d99d92dccb63b3b5145c87597fece3d7f0478
Rami Jarrar <rami@dreamshardstudios.com>
2013-09-28 22:28:34 +1000

    Secured publications to allow operators and users safe access to call data.

--- src/server/publications.js
+++ src/server/publications.js
@@ -1,3 +1,8 @@
-Meteor.publish('calls', function() {
-  return Calls.find();
+Meteor.publish('calls', function(userID) {
+  if (this.userId) {
+    return Calls.find({},{fields: {user: 0}});
+  } else {
+    check(userID, String);
+    return Calls.find({user:userID}, [{fields: {operator: 0, status: 0}},{ sort: [["timestamp","asc"]] }]);
+  };
 });
