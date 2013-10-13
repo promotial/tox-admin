d81bc1a99b1140d76bd0fb14217003c97ab24af1
Rami Jarrar <rami@dreamshardstudios.com>
2013-10-13 17:48:21 +1100

    Added close icon to settings box

--- app/client/views/app/settings.js
+++ app/client/views/app/settings.js
@@ -1,3 +1,9 @@
+Template.settings.events({
+  'click .settings-close': function() {
+    Session.set("settings",false);
+  }
+});
+
 Template.settings.helpers({
   settings: function() {
     return Session.get("settings");
